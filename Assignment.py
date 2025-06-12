52c2df3283171cd72f0e8ff91c3106627c395b29
Microbeast07MB <arshkhandelwal25@gmail.com>
2025-06-12 21:49:22 +0530

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,31 @@
+#Assignment 1
+
+
+a=input("What is your 1st number: ")
+b=input("What is your 2nd number: ")
+
+a=float(a)
+b=float(b)
+
+
+print("Addition", a+b)
+print("Subtraction", a-b)
+print("Multiplication", a*b)
+print("Division", a/b)    
+
+
+
+
+
+
+
+
+#Assignment 2
+
+
+x=input("what is your first name: ")
+y=input("what is your last name: ")
+
+
+print("Welcome to the great journey", x, y)
+
