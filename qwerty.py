c0cb536158750ae4098906a2fdda15c37f25cd20
Microbeast07MB <arshkhandelwal25@gmail.com>
2025-06-14 20:07:24 +0530

    Add files via upload

---
+++ qwerty.py
@@ -0,0 +1,37 @@
+# a=float(input("Enter a number: "))
+#
+# if a%2 == 0:
+#     print("This number is even")
+#
+# else:
+#     print("This number is odd")
+
+
+
+x=0
+
+for i in range(1,51):
+    x += i
+
+print("Total sum from 1 to 50 is ", x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
