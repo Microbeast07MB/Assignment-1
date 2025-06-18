162b41b060d81aba4d577b925b851af49b80f878
Microbeast07MB <arshkhandelwal25@gmail.com>
2025-06-18 16:45:00 +0530

    Add files via upload

--- qwerty.py
+++ qwerty.py
@@ -1,37 +1,24 @@
- a=float(input("Enter a number: "))
-
- if a%2 == 0:
-     print("This number is even")
-
- else:
-     print("This number is odd")
-
-
-#--------------------------------------------------------
-x=0
-
-for i in range(1,51):
-    x += i
-
-print("Total sum from 1 to 50 is ", x)
+n=int(input("Enter your number: "))
 
 
 
+def factorial(n):
+    if n<2:
+        return 1
+    else:
+        return n * factorial(n-1)
 
 
+print("Factorial of",n, "is: ",factorial(n))
 
 
+#--------------------------------------------------------------------------------------------------
 
+import math
 
+x=float(input("Enter Number: "))
 
-
-
-
-
-
-
-
-
-
-
+print("Square Root: ",math.sqrt(x))
+print("Logarithm: ",math.log(x))
+print("Sine: ",math.sin(x))
 
