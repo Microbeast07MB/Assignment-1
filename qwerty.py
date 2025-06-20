65b29e4d8e96ad5e5be590d49e067f25a8ba0bc6
Microbeast07MB <arshkhandelwal25@gmail.com>
2025-06-20 18:38:14 +0530

    Add files via upload

--- qwerty.py
+++ qwerty.py
@@ -1,24 +1,42 @@
-n=int(input("Enter your number: "))
+file1=open("sample.txt", "w")
+w=file1.write("It is a sample text file. \nIt consist of multiple lines.")
+print(w)
+file1.close()
+
+try:
+    with open("sample.txt", 'r') as file:
+        print("Reading file content:")
+        for index, line in enumerate(file, start=1):
+            print(f"Line {index}: {line.strip()}")
+except FileNotFoundError:
+    print(f"Error: The file '{"sample.txt"}' was not found.")
+
+
+#----------------------------------------------------------------------
+
+w2 = input("Enter text to write to the file: ")
+with open("output.txt", "w") as file:
+    file.write(w2 + "\n")
+print("Data successfully written to output.txt.")
+
+a = input("\nEnter additional text to append: ")
+with open("output.txt", "a") as file:
+    file.write(a + "\n")
+print("Data successfully appended.")
+
+print("\nFinal content of output.txt:")
+with open("output.txt", "r") as file:
+    r = file.read()
+    print(r)
 
 
 
-def factorial(n):
-    if n<2:
-        return 1
-    else:
-        return n * factorial(n-1)
 
 
-print("Factorial of",n, "is: ",factorial(n))
 
 
-#--------------------------------------------------------------------------------------------------
 
-import math
 
-x=float(input("Enter Number: "))
 
-print("Square Root: ",math.sqrt(x))
-print("Logarithm: ",math.log(x))
-print("Sine: ",math.sin(x))
+
 
