e8ac5e2b68d141012478b3cf54e1de3c86b5fd1b
Microbeast07MB <arshkhandelwal25@gmail.com>
2025-06-26 08:37:38 +0530

    Add files via upload

---
+++ qwerty2.py
@@ -0,0 +1,20 @@
+Names_marks={"Alice": 85 ,"Okarun": 67 , "Videl": 88 , "Luffy": 34 , "Robin": 98 }
+
+a=input("What is the student name: ").title()
+
+if a in Names_marks:
+    print("Marks obtained by",a,"is" ,Names_marks[a])
+
+else:
+    print("Student not found")
+
+
+#------------------------------------------------------------------
+
+natural_numbers=[1,2,3,4,5,6,7,8,9,10]
+
+a=natural_numbers[:5]
+print("Extracted first five elements: ",a )
+
+a.reverse()
+print("Reversed extracted elements",a)
