53e910d39845899772f74d21cdfc1cf0dc0d2328
Microbeast07MB <arshkhandelwal25@gmail.com>
2025-06-26 21:27:33 +0530

    Add files via upload

--- qwerty2.py
+++ qwerty2.py
@@ -12,9 +12,10 @@ else:
 #------------------------------------------------------------------
 
 natural_numbers=[1,2,3,4,5,6,7,8,9,10]
+print("Original list: ",natural_numbers)
 
 a=natural_numbers[:5]
 print("Extracted first five elements: ",a )
 
 a.reverse()
-print("Reversed extracted elements",a)
+print("Reversed extracted elements: ",a)
